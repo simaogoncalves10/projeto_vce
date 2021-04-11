dfdbe8d2e2398052c3b6f64a428a7ed0fa424275
HFaria10 <49321479+HFaria10@users.noreply.github.com>
2021-04-11 14:32:02 +0100

    Atualização de pequenas coisas

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -14,28 +14,14 @@ class UserAccountManager(BaseUserManager):
 
         return user
 
-    def create_superuser(self, email, first_name, last_name, password):
-        
-        user = self.create_user(email, first_name, last_name, password)
- 
-        user.is_superuser = True
-        user.staff = True
-        user.save()
-        
-        return user
-
-
 class User(AbstractBaseUser, PermissionsMixin): 
     email = models.EmailField(max_length=255, unique=True)
     first_name = models.CharField(max_length=255, null=False, default=" ")
     last_name = models.CharField(max_length=255, null=False, default=" ")
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-
-    #medic_no = 0
-    #medic_yes = 1
-    #medic_choices = [(medic_no, 'No'), (medic_yes, 'Yes')]
-    #medic = models.IntegerField(choices=medic_choices, default=1)
+    is_medic = models.BooleanField(default=False)
+    is_specialist = models.BooleanField(default=False)
 
     objects = UserAccountManager()
 
@@ -43,4 +29,4 @@ class User(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = ['first_name', 'last_name']
 
     def __str__(self):
-        return self.first_name
+        return self.email
--- backend/pacients/models.py
+++ backend/pacients/models.py
@@ -8,11 +8,11 @@ class Pacient(models.Model):
     birth_date = models.DateField(null=False)
     total_exams = models.IntegerField(unique=True, null=False)
 
-    gender_male = 0
-    gender_female = 1
-    gender_not_to_say = 2
-    gender_choices = [(gender_male, 'Male'), (gender_female, 'Female'), (gender_not_to_say, 'Prefer not to say')]
-    gender = models.IntegerField(choices=gender_choices)
+    #gender_male = 0
+    #gender_female = 1
+    #gender_not_to_say = 2
+    #gender_choices = [(gender_male, 'Male'), (gender_female, 'Female'), (gender_not_to_say, 'Prefer not to say')]
+    #gender = models.IntegerField(choices=gender_choices)
     
     remarks = models.TextField(max_length=1000, null=False)
 
