cc59edcdfe8ded1cdf0c8b1338c0761466b7fb8a
João Pedro Matos Ribeiro Soares <jpmrs1313@gmail.com>
2021-04-20 14:50:02 +0100

    query choices defined

--- backend/als/models.py
+++ backend/als/models.py
@@ -4,3 +4,19 @@ class AL(models.Model):
     name = models.CharField(max_length=50, null=False, blank=True)
     n_instances = models.IntegerField(null=False, default=10)
     accuracy_goal = models.FloatField(null=False, default=85)
+
+    technics = (
+        ('RandomSampling', 'RandomSampling'),
+        ('UncertaintySampling', 'UncertaintySampling'),
+        ('ClusterBasedSampling', 'ClusterBasedSampling'),
+        ('OutlierSampling', 'OutlierSampling'),
+        ('RepresentativeSampling', 'RepresentativeSampling'),
+        ('UncertaintyWithClusteringSampling', 'UncertaintyWithClusteringSampling'),
+        ('UncertaintyWithModelOutliersSampling', 'UncertaintyWithModelOutliersSampling'),
+        ('RepresentativeWithClusteringSampling','RepresentativeWithClusteringSampling'),
+        ('HighestEntropyClusteringSampling', 'HighestEntropyClusteringSampling'),
+        ('UncertaintyWithRepresentativeSampling', 'UncertaintyWithRepresentativeSampling'),
+        ('HighestEntropyUncertaintySampling', 'HighestEntropyUncertaintySampling'),
+        ('OutliersWithRepresentativeSampling', 'OutliersWithRepresentativeSampling'),
+    )
+    query_technic = models.CharField(max_length=50, choices=technics, default='RandomSampling')
